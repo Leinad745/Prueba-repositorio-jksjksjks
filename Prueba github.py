52f57dca748d08055dd8f1caac44a0205a149b79
Leinad745 <da.corderom@duocuc.cl>
2024-05-17 09:08:26 -0400

    Version 1.0

---
+++ Prueba github.py
@@ -0,0 +1,17 @@
+print('Hola')
+
+#COMANDO DE GITHUB (HACER CADA CLASE en la terminal) (USAR EN CARPETAS)
+
+#git config --global user.name "usuariogithub"
+
+#git config --global user.email Correo
+
+#Añadir carpetas dentro del repositorio de Github
+
+# echo "# Prueba-repositorio-jksjksjks" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin "link del repositorio"
+# git push -u origin main
